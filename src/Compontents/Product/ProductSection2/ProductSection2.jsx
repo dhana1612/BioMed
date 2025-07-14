12e5fa759218b09e0bff1b46818115aff3fb4781
Dhana <dhana121622@gmail.com>
2025-07-15 02:56:38 +0530

    Fortheen commit

--- src/Compontents/Product/ProductSection2/ProductSection2.jsx
+++ src/Compontents/Product/ProductSection2/ProductSection2.jsx
@@ -4,21 +4,21 @@ import Infrastructure3 from '../../InfrastructurePage/Infrastructure3'
 import Productsection12 from '../ProductSection1/Productsection12'
 import Productsection21 from './Productsection21'
 import ProductSection22 from './ProductSection22'
-import Head from 'next/head';
+import { Helmet } from 'react-helmet'
 
 export default function ProductSection2() {
   return (
     <>
 
-<Head>
+    <Helmet>
         <title>Structure of Collagen | Biomed International</title>
         <meta name="description" content="Learn about the chemical structure, formula, and key capabilities of Collagen and Biomed International's manufacturing processes." />
         <meta property="og:title" content="Structure of Collagen | Biomed International" />
         <meta property="og:description" content="Biomed International’s advanced manufacturing facilities and research for collagen and related supplements." />
         <meta property="og:image" content="https://yourdomain.com/preview.jpg" />
         <meta property="og:type" content="website" />
-      </Head>
-
+      </Helmet>
+      
     <Productsectionhero/>
     <Infrastructure3/>
     <Productsection21/>
